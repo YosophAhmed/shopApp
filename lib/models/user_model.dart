f94f553011f036fe1659f5d5a16170b222d68e80
Yosoph Ahmed <devyosoph@gmail.com>
2022-11-23 06:51:19 +0200

    User model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel{
+  final String name;
+  final String password;
+  final String phone;
+
+  UserModel({
+    required this.name,
+    required this.password,
+    required this.phone,
+});
+}
